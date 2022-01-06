b154d64e7a7a3de10e09c0d86f70fc4d813cf4d2
Asad <92751223+UsmonovAsad@users.noreply.github.com>
2022-01-06 15:35:03 +0000

    Project Finished

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const request = axios.create({
 	baseURL: "https://youtube.googleapis.com/youtube/v3/",
 	params : {
-		key: "AIzaSyAbF7N7fuYzSytJwWRPtoM68occblR-sRc"
+		key: // Your Youtube Api Key
 	}
 });
 
