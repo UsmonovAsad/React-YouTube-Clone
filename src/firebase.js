f4c6fcff31f7ce99a568ebe54b88a19a57d2138e
Asad <92751223+UsmonovAsad@users.noreply.github.com>
2022-01-06 15:34:07 +0000

    Project Finished

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDnzsER3QZGzRXAlc8YLjxPTvtLc7szR4Q",
-    authDomain: "you-tube-theone.firebaseapp.com",
-    projectId: "you-tube-theone",
-    storageBucket: "you-tube-theone.appspot.com",
-    messagingSenderId: "789234935481",
-    appId: "1:789234935481:web:55b091d3c3460be4f7dd68"
+	// Initialize Firebase
 }
 
 firebase.initializeApp(firebaseConfig);
